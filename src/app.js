bb4438bc3ebb92ffadd2cfd1353afdded864a464
Juan San Cristobal <130845154+Juan-MSCC@users.noreply.github.com>
2026-01-10 17:51:29 +0000

    Finished basic project, without the extra challenges

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardNumbers = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+  const cardSuit = ['spade', 'club', 'heart', 'diamond']
+  
+  let cardNumber = document.querySelector('#cardNumber')
+  cardNumber.innerHTML = cardNumbers[Math.floor(Math.random() * cardNumbers.length)]
+
+  let topIcon = document.querySelector('.top-icon').querySelector('i')
+  let bottomIcon = document.querySelector('.bottom-icon').querySelector('i')
+
+  let randomSuit = cardSuit[Math.floor(Math.random() * cardSuit.length)]
+  console.log(randomSuit)
+  topIcon.classList.add(randomSuit)
+  bottomIcon.classList.add(randomSuit)
 };
